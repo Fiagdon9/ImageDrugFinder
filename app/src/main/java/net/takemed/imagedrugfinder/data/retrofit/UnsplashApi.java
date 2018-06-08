b377723361058f049d4a6afd6ca8a99961a0e0f9
Stas Kaloev <st.dz.fiagdon@gmail.com>
2018-06-08 12:46:21 +0400

    add interface

--- app/src/main/java/net/takemed/imagedrugfinder/data/retrofit/UnsplashApi.java
+++ app/src/main/java/net/takemed/imagedrugfinder/data/retrofit/UnsplashApi.java
@@ -10,5 +10,5 @@ import retrofit2.http.QueryName;
 public interface UnsplashApi {
 //    @GET("/search/photos?client_id=11bee5ebf6e392875ecee649ef9f4a6953e3f4bb623112d9bc3fbf4651e277d8&query=computer")
     @GET("/search/photos")
-    Call<JsonObject> searchPhotos(@Query("clientId") String clientId, @Query("query") String query);
+    Call<JsonObject> searchPhotos(@Query("client_id") String clientId, @Query("query") String query);
 }
