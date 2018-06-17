a29789ad322b38aeba6e658415634cad6e4c68bd
Stas Kaloev <stanislavkaloev@bk.ru>
2018-06-17 16:20:47 +0400

    With StartActivity layout

--- app/src/main/java/net/takemed/imagedrugfinder/ui/activity/SplashActivity.java
+++
@@ -1,7 +0,0 @@
-package net.takemed.imagedrugfinder.ui.activity;
-
-
-import android.support.v7.app.AppCompatActivity;
-
-public class SplashActivity extends AppCompatActivity{
-}
--- app/src/main/java/net/takemed/imagedrugfinder/ui/activity/StartActivity.java
+++ app/src/main/java/net/takemed/imagedrugfinder/ui/activity/StartActivity.java
@@ -1,6 +1,27 @@
 package net.takemed.imagedrugfinder.ui.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import net.takemed.imagedrugfinder.R;
 
 public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+
+        ProgressBar pb = findViewById(R.id.progressBarImages);
+
+        ((TextView)findViewById(R.id.tvProgressNum))
+                .setText(pb.getProgress() + "/" + pb.getMax());
+    }
+
+    public void onStartActivity(View view) {
+        startActivity(new Intent(StartActivity.this, QuizActivity.class));
+    }
 }
